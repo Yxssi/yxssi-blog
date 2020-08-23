e4f18f7664dc65157d905749b08f4883e4e11cd9
Yessi Munguengui <yessimunguengui@gmail.com>
2020-08-24 00:07:37 +0200

    fix footbar

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import { RiHeart2Line } from "react-icons/ri"
 const Footer = () => (
   <footer className="site-footer">
     <div className="container">
-      <p>YESSI-Y-MUNGUENGUI Yessi- 2020</p>
+      <p>YESSI-Y-MUNGUENGUI Yessi - 2020</p>
     </div>
   </footer>
 )
